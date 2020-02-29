2b73486d25a8b1ec1d5802f6faf5452ed0488f66
shirai02 <shirai-kazuki-yn@ynu.jp>
2020-02-29 15:24:43 +0900

    トランザクション生成＋マイニングテスト

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import hashlib
+import pprint
+import BlockChain
+
 
 def main():
-    dat = 'python'
-    hs = hashlib.sha256(dat.encode()).hexdigest()
-    print(hs)
+    usr1 = "a"
+    usr2 = "b"
+    usr3 = "c"
+
+    blockChain = BlockChain.BlockChain("minor")
+
+    blockChain.add_transaction(usr1,usr2,30)
+    blockChain.add_transaction(usr2,usr3,10)
+    blockChain.add_transaction(usr3,usr1,10)
+
+    # blockChain.create_block(0,blockChain.hash(blockChain.chain[-1]))
+    blockChain.mining()
+    # pprint.pprint(blockChain.transaction_pool)
+    pprint.pprint(blockChain.chain)
+
 
 if __name__ == '__main__':
-  main()
+    main()
